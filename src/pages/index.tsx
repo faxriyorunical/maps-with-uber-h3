4fe3544c878190449908db33610a756bf5b94514
808vita <its808vita@gmail.com>
2024-02-04 16:07:10 +0530

    cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,12 @@
-import { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 // import MapComponent from "@/components/MapComponent";
-
-
 const LazyMap = dynamic(() => import("@/components/MapComponent"), {
   ssr: false,
 });
 
 export default function Home() {
   return (
-    <main
-      className='leaflet-container'
-    >
+    <main className="leaflet-container">
       {/* { <MapComponent />} */}
       <LazyMap />
     </main>
