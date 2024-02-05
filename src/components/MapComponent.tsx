ea9ddb6033c6ad6f0b4d0b65f4d602f9500116be
808vita <its808vita@gmail.com>
2024-02-06 03:43:33 +0530

    refactor - h3 utils into new file

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -25,6 +25,7 @@ import { leafletConfig, markerPath } from "@/utils/leafletConfig";
 import { routingUtilExported } from "@/utils/routingUtil";
 import { geocodingUtilExported } from "@/utils/geocodingUtil";
 import * as h3 from "h3-js";
+import { h3indexUtil, hexBoundaryUtil, hexCenterCoordinatesUtil } from "@/utils/hexUtils";
 // const markerPath = "/marker-icon.png";
 
 /**
@@ -163,85 +164,6 @@ const RoutingMachineController = (props: any) => {
     resetWaypointStates();
   };
 
-  /**
-   *
-   * @param res
-   * @param latLng
-   * @returns h3index string
-   *
-   * util to generate h3index from latlng
-   *
-   * Convert a lat/lng point to a hexagon index at resolution 10
-   *
-   * 0 (continental) to res 15 (1 square meter). Res 9 is roughly a city block
-   *
-   * setting default to 11
-   */
-  const h3indexUtil = (
-    res = 11,
-    latLng: L.LatLng,
-    setH3IndexList: (
-      value: React.SetStateAction<any[] | [] | [string[]]>
-    ) => void
-  ) => {
-    // Convert a lat/lng point to a hexagon index at resolution 10
-    // 0 (continental) to res 15 (1 square meter). Res 9 is roughly a city block
-    const h3Index: string = h3.latLngToCell(latLng.lat, latLng.lng, 11);
-
-    console.log(h3Index, "h3Index");
-
-    setH3IndexList((prev) => [...prev, h3Index]);
-
-    return h3Index;
-  };
-
-  /**
-   *
-   * @param h3Index
-   * @returns [latlng] | h3.CoordPair
-   *
-   * Get the center of the hexagon
-   */
-  const hexCenterCoordinatesUtil = (
-    h3Index: string,
-    setHexCenterCoordinatesList: (
-      value: React.SetStateAction<any[] | [] | [L.LatLng[]]>
-    ) => void
-  ) => {
-    // Get the center of the hexagon
-    const hexCenterCoordinates: h3.CoordPair = h3.cellToLatLng(h3Index);
-
-    console.log(hexCenterCoordinates, "hexCenterCoordinates");
-
-    setHexCenterCoordinatesList((prev) => [...prev, hexCenterCoordinates]);
-
-    return hexCenterCoordinates;
-  };
-
-  /**
-   *
-   * @param h3Index
-   * @param hexBoundaryList
-   * @param setHexBoundaryList
-   *
-   * generate hex boundaries [[lat,lng]]
-   *
-   * store to hexBoundaryList
-   *
-   *
-   */
-  const hexBoundaryUtil = (
-    h3Index: string,
-    setHexBoundaryList: (
-      value: React.SetStateAction<any[] | [] | [L.LatLng[]]>
-    ) => void
-  ) => {
-    // Get the vertices of the hexagon
-    const hexBoundary: h3.CoordPair[] = h3.cellToBoundary(h3Index);
-    console.log(hexBoundary, "hexBoundary");
-
-    setHexBoundaryList((prev) => [...prev, hexBoundary]);
-  };
 
   /**
    *
---
+++ src/utils/hexUtils.ts
@@ -0,0 +1,81 @@
+import * as h3 from "h3-js";
+
+  /**
+   *
+   * @param res
+   * @param latLng
+   * @returns h3index string
+   *
+   * util to generate h3index from latlng
+   *
+   * Convert a lat/lng point to a hexagon index at resolution 10
+   *
+   * 0 (continental) to res 15 (1 square meter). Res 9 is roughly a city block
+   *
+   * setting default to 11
+   */
+  export const h3indexUtil = (
+    res = 11,
+    latLng: L.LatLng,
+    setH3IndexList: (
+      value: React.SetStateAction<any[] | [] | [string[]]>
+    ) => void
+  ) => {
+    // Convert a lat/lng point to a hexagon index at resolution 10
+    // 0 (continental) to res 15 (1 square meter). Res 9 is roughly a city block
+    const h3Index: string = h3.latLngToCell(latLng.lat, latLng.lng, 11);
+
+    console.log(h3Index, "h3Index");
+
+    setH3IndexList((prev) => [...prev, h3Index]);
+
+    return h3Index;
+  };
+
+  /**
+   *
+   * @param h3Index
+   * @returns [latlng] | h3.CoordPair
+   *
+   * Get the center of the hexagon
+   */
+  export const hexCenterCoordinatesUtil = (
+    h3Index: string,
+    setHexCenterCoordinatesList: (
+      value: React.SetStateAction<any[] | [] | [L.LatLng[]]>
+    ) => void
+  ) => {
+    // Get the center of the hexagon
+    const hexCenterCoordinates: h3.CoordPair = h3.cellToLatLng(h3Index);
+
+    console.log(hexCenterCoordinates, "hexCenterCoordinates");
+
+    setHexCenterCoordinatesList((prev) => [...prev, hexCenterCoordinates]);
+
+    return hexCenterCoordinates;
+  };
+
+  /**
+   *
+   * @param h3Index
+   * @param hexBoundaryList
+   * @param setHexBoundaryList
+   *
+   * generate hex boundaries [[lat,lng]]
+   *
+   * store to hexBoundaryList
+   *
+   *
+   */
+  export const hexBoundaryUtil = (
+    h3Index: string,
+    setHexBoundaryList: (
+      value: React.SetStateAction<any[] | [] | [L.LatLng[]]>
+    ) => void
+  ) => {
+    // Get the vertices of the hexagon
+    const hexBoundary: h3.CoordPair[] = h3.cellToBoundary(h3Index);
+    console.log(hexBoundary, "hexBoundary");
+
+    setHexBoundaryList((prev) => [...prev, hexBoundary]);
+  };
