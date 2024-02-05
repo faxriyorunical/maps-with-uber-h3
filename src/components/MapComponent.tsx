e7e917bec9183bc64a949a57b0a7d62c52788022
808vita <its808vita@gmail.com>
2024-02-05 18:39:50 +0530

    clean up

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -31,59 +31,6 @@ import { geocodingUtilExported } from "@/utils/geocodingUtil";
  */
 let routeControl2: any = null;
 
-// const geocodingUtil = (
-//   mapRef: any,
-//   latLng: any,
-//   reverseCodedWaypoints: any,
-//   setReverseCodedWaypoints: any
-// ) => {
-//   // trying geocoding -- this works
-//   //@ts-ignore
-//   let geocoder = new L.Control.Geocoder.nominatim();
-
-//   // let scalingAccuracy = mapRef.options.crs.scale(mapRef.getZoom())
-//   let scalingAccuracy = mapRef?.options.crs.scale(20);
-//   console.log(scalingAccuracy, "scalingAccuracy");
-
-//   let reverseCodedValue = "";
-
-//   geocoder.reverse(latLng, scalingAccuracy, function (results: any) {
-//     let address = "";
-//     if (results?.length > 0) {
-//       let reverseCoded = results[0];
-//       console.log(reverseCoded, "reverseCoded");
-//       address = reverseCoded.name;
-//     }
-
-//     if (address != "") {
-//       reverseCodedValue = address;
-//       setReverseCodedWaypoints([...reverseCodedWaypoints, reverseCodedValue]);
-//     }
-
-//     if (address == "") {
-//       // latlng address fallback - if returns empty string set lat lng template string
-//       reverseCodedValue = `Lat: ${latLng.lat} , Lng: ${latLng.lng}`;
-//       setReverseCodedWaypoints([...reverseCodedWaypoints, reverseCodedValue]);
-//     }
-
-//     if (address != "") {
-//       /**
-//        * geocoding string address to coordinates
-//        */
-//       geocoder.geocode(address, function (results: any) {
-//         console.log(results);
-//         if (results?.length > 0) {
-//           let latLngoof = new L.LatLng(
-//             results[0].center.lat,
-//             results[0].center.lng
-//           );
-//           console.log(results);
-//           console.log(latLngoof, "latLngoof");
-//         }
-//       });
-//     }
-//   });
-// };
 
 const RoutingMachineController = (props: any) => {
   const [waypoints, setWaypoints] = useState<L.LatLng[] | [] | any>([]);
@@ -134,13 +81,6 @@ const RoutingMachineController = (props: any) => {
       if (!showMenu) {
         showMenuHandler();
       }
-
-      // geocodingUtil(
-      //   mapRef,
-      //   latLng,
-      //   reverseCodedWaypoints,
-      //   setReverseCodedWaypoints
-      // );
 
       geocodingUtil(latLng);
     },
