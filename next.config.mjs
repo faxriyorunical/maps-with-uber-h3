5827b3e1986758bf566bd7035d0b495f7d12c86d
808vita <its808vita@gmail.com>
2024-02-08 23:35:00 +0530

    static site config | git ignore for docs & dist folders | replaced favicon

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  //create static site bundles
+  // for hosting on gcp storage bucket
+  output: "export",
+  distDir: "dist",
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 export default nextConfig;
